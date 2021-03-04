6f05bc816593a725d9323c2fd00a8f8a5498aaa0
saralpowers <63485240+saralpowers@users.noreply.github.com>
2021-03-03 16:01:55 -0800

    Update script.js

--- lab5-withvalidation/js/script.js
+++ lab5-withvalidation/js/script.js
@@ -4,17 +4,17 @@ let mpg;
 let again = 'y';
 
 do {
-    miles = parseFloat(prompt('Enter miles driven'));
-    gallons = parseFloat(prompt('How many gallons does your car\'s tank hold'));
+    miles = parseFloat(prompt('Enter miles driven: '));
+    gallons = parseFloat(prompt('Enter how many gallons of gas to fill tank: '));
     if (!isNaN(miles) && miles > 0 && !isNaN(gallons) && gallons > 0) {
         mpg = miles / gallons;
-        alert(`Your car gets ${mpg.toFixed(2)} miles per gallon.`);
+        document.write(`Your car get ${mpg.toFixed(2)} miles per gallon.`);
     } else {
-        alert('One or both entries are invalid.');
+        document.write('One or both entries are invalid.');
     }
     again = prompt('Run application again? (y or n)', 'y');
+    //VALIDATION:prompt reappears until the user enters a valid entry (y or n)
     while (again !== 'y' && again !== 'n') {
         again = prompt('Run application again? (y or n)', 'y');
     }
 } while (again === 'y');
-alert('Application has exited.');
